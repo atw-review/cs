6b9da98c2a312893cd6ff941fadb9ba4133faf74
atwpreview <avexpolly@gmail.com>
2018-01-23 01:56:05 +0800

    refine gulpfile and gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,50 @@
 var gulp               = require('gulp'),
+    print              = require('gulp-print'),
     clean              = require('gulp-clean'),
-    sass               = require('gulp-sass'),
+    imagemin           = require('gulp-imagemin'),
+    compass            = require('gulp-compass'),
     autoprefixer       = require('gulp-autoprefixer'),
     concat             = require('gulp-concat'),
     uglify             = require('gulp-uglify'),
     rename             = require('gulp-rename'),
     plumber            = require('gulp-plumber'),
-    fileinclude        = require('gulp-file-include'),
-    rev                = require('gulp-revm'),
-    revCollector       = require('gulp-revm-collector'),
+    fileInclude        = require('gulp-file-include'),
+    rev                = require('gulp-revm'),            // rev: json 版號
+    revCollector       = require('gulp-revm-collector'),  // revCollector: 資源路徑更改
     browserSync        = require('browser-sync'),
     reload             = browserSync.reload;
 
-// rev: json 版號
-// revCollector: 資源路徑更改
 
+// path
 var PATH_SRC_STYLE = 'src/assets/sass/**/*.sass';
 var PATH_SRC_JS    = 'src/assets/js/*.js';
+var PATH_SRC_IMG   = 'src/assets/img/**';
 var PATH_SRC_HTML  = 'src/**/*.html';
 var PATH_DES_STYLE = 'public/assets/css/';
 var PATH_DES_JS    = 'public/assets/js/';
+var PATH_DES_IMG   = 'public/assets/img/';
 var PATH_DES_HTML  = 'public/';
 var PATH_REV_JSON  = 'rev/**/*.json';
+
+
+// gulp.task('print', function() {
+//   gulp.src(PATH_SRC_IMG)
+//     .pipe(print())
+// });
+
 
 gulp.task('clean-css', function() {
   return gulp.src(PATH_DES_STYLE, {read: false})
     .pipe(clean());
 });
 
-// compile sass
-gulp.task('sass', ['clean-css'], function() {
-  return gulp.src(PATH_SRC_STYLE)
-    .pipe(plumber())
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(rev())
-    .pipe(gulp.dest(PATH_DES_STYLE))
-    .pipe(rev.manifest())
-    .pipe(gulp.dest('rev/css'));
-    // .pipe(reload({stream: true}));
-});
 
 var vendorcss = [
+  'node_modules/bootstrap/dist/css/bootstrap.min.css',
   'node_modules/aos/dist/aos.css',
   'node_modules/flatpickr/dist/flatpickr.min.css'
 ]
+
 
 gulp.task('vendor-css', function() {
   return gulp.src(vendorcss)
@@ -57,13 +52,63 @@ gulp.task('vendor-css', function() {
     .pipe(gulp.dest(PATH_DES_STYLE));
 });
 
+
+// gulp.task('sass', ['clean-css'], function() {
+//   return gulp.src(PATH_SRC_STYLE)
+//     .pipe(plumber())
+//     .pipe(sass({
+//       outputStyle: 'compressed'
+//     }))
+//     .pipe(autoprefixer({
+//       browsers: ['last 2 versions'],
+//       cascade: false
+//     }));
+// });
+
+
+// compile sass
+// gulp.task('compass', ['clean-css'], function() {
+gulp.task('compass', function() {
+  gulp.src(PATH_SRC_STYLE)
+    .pipe(plumber())
+    .pipe(compass({
+      config_file: 'config.rb',
+      css: 'public/assets/css',  // 輸出位置
+      sass: 'src/assets/sass'    // 來源路徑
+    }))
+    .pipe(rev())
+    .pipe(gulp.dest(PATH_DES_STYLE))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('rev/css'));
+});
+
+
 gulp.task('clean-scripts', function() {
   return gulp.src(PATH_DES_JS, {read: false})
     .pipe(clean());
 });
 
+
+var vendorjs = [
+  'node_modules/jquery/dist/jquery.slim.min.js',
+  'node_modules/popper.js/dist/umd/popper.min.js',
+  'node_modules/bootstrap/dist/js/bootstrap.min.js',
+  'node_modules/jquery.easing/jquery.easing.min.js',
+  'node_modules/aos/dist/aos.js',
+  'node_modules/scrolltofixed/jquery-scrolltofixed-min.js'
+]
+
+
+gulp.task('vendor-js', function() {
+  return gulp.src(vendorjs)
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(PATH_DES_JS));
+});
+
+
 // concatenate & minify JS (MD5 process)
-gulp.task('scripts', ['clean-scripts'], function() {
+// gulp.task('scripts', ['clean-scripts'], function() {
+gulp.task('scripts', function() {
   return gulp.src(PATH_SRC_JS)
     .pipe(plumber())
     .pipe(concat('all.js'))
@@ -73,36 +118,31 @@ gulp.task('scripts', ['clean-scripts'], function() {
     .pipe(rev())
     .pipe(gulp.dest(PATH_DES_JS))
     .pipe(rev.manifest())
-    .pipe(gulp.dest('rev/js'));
-    // .pipe(reload({stream: true}));
+    .pipe(gulp.dest('rev/js'))
+    .pipe(reload({stream: true}));
 });
 
-var vendorjs = [
-  'node_modules/jquery/dist/jquery.min.js',
-  'node_modules/jquery.easing/jquery.easing.min.js',
-  'node_modules/bootstrap/dist/js/bootstrap.min.js',
-  'node_modules/aos/dist/aos.js',
-  'node_modules/scrolltofixed/jquery-scrolltofixed-min.js'
-]
 
-gulp.task('vendor-js', function() {
-  return gulp.src(vendorjs)
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest(PATH_DES_JS));
+// optimize images
+gulp.task('image', function () {
+  gulp.src(PATH_SRC_IMG)
+    .pipe(imagemin())
+    .pipe(gulp.dest(PATH_DES_IMG));
 });
+
 
 // reversion css/js deps on task [sass, scripts]
-gulp.task('rev-collector', ['sass', 'scripts'] ,function(cb) {
+gulp.task('rev-collector', ['compass', 'scripts'] ,function(cb) {
   gulp.src([PATH_REV_JSON, PATH_SRC_HTML])
     .pipe(revCollector({
       replaceReved: true
     }))
-    .pipe(fileinclude({
+    .pipe(fileInclude({
       prefix: '@@',
       basepath: '@file'
     }))
     .pipe(gulp.dest(PATH_DES_HTML))
-    .pipe(reload({stream: true}));
+    .pipe(reload({stream: true}))
     cb();
 });
 
@@ -118,6 +158,8 @@ gulp.task('browser-sync', function() {
 // watch
 gulp.task('watch',function() {
   gulp.watch([PATH_SRC_STYLE, PATH_SRC_JS, PATH_SRC_HTML], ['rev-collector']);
+  gulp.watch([PATH_SRC_IMG], ['image']);
 });
 
-gulp.task('default', ['rev-collector', 'vendor-css', 'vendor-js', 'browser-sync', 'watch']);
+// gulp.task('default', ['rev-collector', 'vendor-css', 'vendor-js', 'browser-sync', 'watch']);
+gulp.task('default', ['clean-css', 'vendor-css', 'clean-scripts', 'vendor-js', 'rev-collector', 'image', 'watch']);
