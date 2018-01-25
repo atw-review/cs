dea01805bc378c6d1aad09990dad672c34fca4f1
atwpreview <avexpolly@gmail.com>
2018-01-25 11:52:40 +0800

    fix navbar / google map and refine images path

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -18,20 +18,21 @@ $(document).ready(function() {
   AOS.init({
     duration: 600,
     easing: 'ease-in-out-sine',
-    delay: 100
+    delay: 100,
+    disable: 'mobile'
   });
 
   // google map
-  // var myLatlng = new google.maps.LatLng(25.051094, 121.5928083);
-  // var mapOptions = {
-  //   zoom: 17,
-  //   center: myLatlng,
-  // }
-  // var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  // var marker = new google.maps.Marker({
-  //     position: myLatlng,
-  //     title:"CHILLSPACE"
-  // });
+  var myLatlng = new google.maps.LatLng(25.051094, 121.5928083);
+  var mapOptions = {
+    zoom: 17,
+    center: myLatlng,
+  }
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      title:"CHILLSPACE"
+  });
   // To add the marker to the map, call setMap();
-  // marker.setMap(map);
+  marker.setMap(map);
 });
